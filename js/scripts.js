79232064488eafb4b8474993a99ad12906dc3e97
lwairore <kwairore@gmail.com>
2019-04-07 11:32:02 +0300

    Add an event listener hold button for player2. Calls the hold() prototype and replaces Die Roll and Roll Total score for player 2 if clicked

--- js/scripts.js
+++ js/scripts.js
@@ -65,4 +65,11 @@ $(document).ready(function () {
         $("#die-roll-1").empty();
         player1.winnerCheck();
     });
+    $("button#player2-hold").click(function (event) {
+        player2.hold();
+        $("#total-score-2").text(player2.cumulativeScore);
+        $("#round-total-2").empty();
+        $("#die-roll-2").empty();
+        player2.winnerCheck();
+    });
 })
